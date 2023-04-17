0a5ee27be4ae905a5dfd4028b9c161adfbc3eadc
YY199116 <y.yusuf@outlook.com>
2023-04-17 17:16:16 +0200

    Experience Added

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -18,7 +18,7 @@ function Skills({ skills }: Props) {
       <h3 className="absolute top-24 p-5 uppercase tracking-[3px] text-gray-500 text-sm">
         Hover over a skill for current profieciency
       </h3>
-      <div className="grid grid-cols-6 gap-4  max-w-screen-lg">
+      <div className="grid grid-cols-4 gap-4  max-w-screen-lg">
         {skills?.slice(0, skills.length / 2).map((skill) => (
           <Skill key={skill._id} skill={skill} />
         ))}
